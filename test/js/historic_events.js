73b6289759e9ffcaa5e21b6f769a683002636629
marcChantebelSyleg <marc.chantebel@syleg.fr>
2026-07-29 17:00:47 +0200

    Optimize historic import : add search bar for organizations, create system to extract datas related to specific tag search

--- test/js/historic_events.js
+++ test/js/historic_events.js
@@ -2,6 +2,7 @@ const historicFileOrganizations = document.getElementById("historicFileOrganizat
 const historicFileAllDatas = document.getElementById("historicFileAllDatas")
 const CallHistoricFileAllDatas = document.getElementById("CallHistoricFileAllDatas");
 const selectAll = document.getElementById("selectAll");
+const writeOptions = document.getElementsByClassName("writeOption");
 
 selectAll.addEventListener("click", (e) => {
     const allInputs = document.querySelectorAll(".organizationSelected input[type='checkbox']");
@@ -11,6 +12,40 @@ selectAll.addEventListener("click", (e) => {
         input.parentNode.setAttribute("data-chosed", "1");
     }
 }, false);
+
+document.getElementById("extractOptionSearch").addEventListener("keyup", (e) => {
+    const extractOptions = document.querySelectorAll("#extractOptions .extractOption");
+    const valueTyped = e.currentTarget.value;
+
+    for (const extractOption of extractOptions) {
+        if (valueTyped == "" || extractOption.getAttribute("data-xml-name").toLowerCase().indexOf(valueTyped.toLowerCase()) != -1)
+            extractOption.classList.remove("d-none");
+        else
+            extractOption.classList.add("d-none");
+    }
+}, false);
+
+document.getElementById("searchOrganizations").addEventListener("keyup", (e) => {
+    const allOrganizations = document.querySelectorAll(".organizationSelected");
+    const valueTyped = e.currentTarget.value;
+
+    for (const organization of allOrganizations) {
+        if (valueTyped == "" || organization.getAttribute("data-name").toLowerCase().indexOf(valueTyped.toLowerCase()) != -1)
+            organization.classList.remove("d-none");
+        else
+            organization.classList.add("d-none");
+    }
+}, false);
+
+for (const writeOption of writeOptions) {
+    writeOption.addEventListener("click", (e) => {
+        const target = e.currentTarget;
+        generateExtractOption(
+            target.parentNode.getElementsByTagName("span")[0].innerText,
+            target.parentNode.getElementsByTagName("div")[0]
+        );
+    }, false);
+}
 
 document.getElementById("CallHistoricFileOrganizations").addEventListener("click", (e) => {
     historicFileOrganizations.click();
@@ -56,7 +91,7 @@ historicFileAllDatas.addEventListener("change", (e) => {
             const result = await window.electronAPIXMLHistoricImportExport.transformHistoric(fr.target.result, true);
 
             if (result.isOk) {
-                const finalResult = generateNewXmlFileForEachCompany(result.json);
+                const finalResult = generateGlobalHistoricFile(result.json);
                 const xmlFinalResult =
                     await window.electronAPIXMLHistoricImportExport.transformHistoric(finalResult, false);
 
